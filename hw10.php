35053448a2a6c81d9d615fa9d865036a59ea43c1
ivan-rancic <irancic@gmail.com>
2022-04-27 23:50:32 +0200

    na drugi način

--- hw10.php
+++ hw10.php
@@ -36,6 +36,31 @@ echo "Danas je " . $days[$g] . "!";
 echo "<br>";
 echo "<br>";
 
+//Zadatak 2 na drugi način
+$y = mt_rand(0,6);
+
+if ($y == 0){
+    echo "Danas je ponedeljak!";
+} else if ($y == 1) {
+    echo "Danas je utorak!";
+}
+  else if ($y == 2) {
+    echo "Danas je sreda!";
+}
+else if ($y == 3) {
+    echo "Danas je četvrtak!";
+}
+else if ($y == 4) {
+    echo "Danas je petak!";
+}
+else if ($y == 5) {
+    echo "Danas je subota!";
+} else {
+    echo "Danas je nedelja!";
+} 
+
+echo "<br>";
+
 //Zadatak 3
 
 $a = 1;
@@ -43,6 +68,15 @@ $b = 2;
 $c = 3;
 $zb = $a + $b +$c;
 echo "Zbir brojeva $a, $b i $c je $zb";
+
+echo "<br>";
+
+//Zadatak 3 na drugi način
+$a = 1;
+$b = 2;
+$c = 3;
+$ar = array ($a, $b, $c);
+echo "Zbir brojeva $a, $b i $c je " . array_sum($ar);
 
 
 
